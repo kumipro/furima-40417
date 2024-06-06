31b782c449728841240041f735258cdcda5a4176
kumipro <ladytamatora@gmail.com>
2024-06-06 23:48:53 +0900

    アクティブハッシュのアソシエーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,8 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping
+  belongs_to :region
+  belongs_to :shipping_date
 end
