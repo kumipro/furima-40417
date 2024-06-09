932f575e5aa0828c906ade8377152ae62c7706bb
kumipro <ladytamatora@gmail.com>
2024-06-09 22:35:01 +0900

    必須条件で出品登録できる(手数料、利益以外)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,8 +8,13 @@ class ItemsController < ApplicationController
   end
 
   def create
-    Item.create(item_params)
-    redirect_to '/'
+    @item = Item.create(item_params)
+   
+     if @item.save
+      redirect_to root_path
+     else
+      render :new, status: :unprocessable_entity
+     end
   end
 
 
