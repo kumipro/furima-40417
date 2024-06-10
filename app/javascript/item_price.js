4896976cdd9f912023abace3056c559097426902
kumipro <ladytamatora@gmail.com>
2024-06-10 19:08:03 +0900

    JavaScript手数料と利益表示の実装

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,10 +1,11 @@
 const priceInput = document.getElementById("item-price");
-priceInput.addEventListener("input", () => {
+ priceInput.addEventListener("input", () => {
   const inputValue = priceInput.value;
-  console.log(inputValue);
+
+const addTaxDom = document.getElementById("add-tax-price");
+  addTaxDom.innerHTML = Math.floor(inputValue * 0.1)
+
+  const profitDom = document.getElementById("profit");
+  profitDom.innerHTML = Math.floor(inputValue - addTaxDom.innerHTML)
 })
 
-
-window.addEventListener('turbo:load', () => {
-  console.log("OK");
-});
