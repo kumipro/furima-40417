6ce43fe46ddde4b3e53a8ee6657ad0ffcf899d29
kumipro <ladytamatora@gmail.com>
2024-06-13 21:41:04 +0900

    編集できる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -28,6 +28,12 @@ class ItemsController < ApplicationController
   def edit
     @item = Item.find(params[:id])
   end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to item_path
+  end
   
 
 
